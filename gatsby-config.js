540738b1950c3952178c16b27dfe5fbdf03c7e7a
dotpegaso <dotpegaso@gmail.com>
2021-03-29 13:23:05 -0300

    update config env

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
-        endpoint: process.env.MAILCHIMP_ENDPOINT,
+        endpoint: process.env.GATSBY_MAILCHIMP_ENDPOINT,
         timeout: 10000,
       },
     },
