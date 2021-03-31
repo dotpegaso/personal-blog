f2e14853af4c712774336559f50cff6cc3331c3d
dotpegaso <dotpegaso@gmail.com>
2021-03-31 02:16:40 -0300

    chore: Add babel-plugin-styled-components to package.json
    It seems that styled-components is having issues to
    build in production with Vercel. Following some topics
    about it, this addition maybe can help to fix the
    build issue.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-json",
+    "babel-plugin-styled-components",
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
